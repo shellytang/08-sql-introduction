82e03ed551544b24006309cfdd4c026fe696e1e2
Shelly Tang <shellytang@gmail.com>
2017-01-11 16:00:49 -0800

    added pg require and worked on sql queries

--- starter-code/public/scripts/article.js
+++ starter-code/public/scripts/article.js
@@ -41,12 +41,16 @@ Article.fetchAll = function(callback) {
     function(results) {
       if (results.rows.length) { // If records exist in the DB
         // TODO: Call loadAll, and pass in the results, then invoke the callback.
+        Article.loadAll(results, )
       } else { // if NO records exist in the DB
         // TODO: Make an ajax call to get the json
         // THEN() iterate over the results, and create a new Article object for each.
           // When that's complete call the insertRecord method for each article you've created.
         // THEN() invoke fetchAll and pass your callback as an argument
         // Don't forget to CATCH() any errors
+        .catch(function(err) {
+          console.log(err);
+        })
       }
     }
   )
